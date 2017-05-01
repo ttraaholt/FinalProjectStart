b741f1640183107de0026878bc7c87cdb2e21308
ttraaholt <ttraaholt@css.edu>
2017-05-01 18:38:38 -0500

    working on login

--- app/src/main/java/ttraaholt/finalprojectstart/MainActivity.java
+++ app/src/main/java/ttraaholt/finalprojectstart/MainActivity.java
@@ -5,11 +5,25 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText etAddNote;
+    TextView tvNotes;
+    ListView lvNotes;
+    Button buttonAddNote;
+    Button buttonDeleteNote;
+
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -19,8 +33,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+        tvNotes = (TextView) findViewById(R.id.textViewNotes);
+        lvNotes = (ListView) findViewById(R.id.listViewNotes);
 
+        userAuthentication();
+        setupAddButton();
+
+    }
+
+    private void userAuthentication() {
+
+        mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() { //initialized mAuthListener
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -35,6 +58,18 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+    }
+
+    private void setupAddButton() {
+        // Set up the button to add a new fish using a seperate activity
+        buttonAddNote = (Button) findViewById(R.id.buttonAdd);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Start up the add fish activity with an intent
+                tvNotes.setText(etAddNote.getText().toString());
+
+            }
+        });
     }
 
     @Override
