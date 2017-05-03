e867752711cf2a4d28f50a8924bb0a49d36659b6
ttraaholt <ttraaholt@css.edu>
2017-05-03 11:32:09 -0500

    working on auth

--- app/src/main/java/ttraaholt/finalprojectstart/SecondActivity.java
+++ app/src/main/java/ttraaholt/finalprojectstart/SecondActivity.java
@@ -3,6 +3,7 @@ package ttraaholt.finalprojectstart;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,27 +13,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonLogin;
-    private Button buttonCreateUser;
+     EditText editTextEmail;
+     EditText editTextPassword;
+     Button buttonLogin;
+     Button buttonCreateUser;
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonCreateUser = (Button) findViewById(R.id.buttonCreateUser);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -44,40 +44,55 @@ public class SecondActivity extends AppCompatActivity {
         //gets the editTextEmail and converts it to a string, and gets the editTextPassword and converts it to a string.
         buttonCreateUser.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //createAccount(editTextEmail.getText().toString(), editTextPassword.getText().toString());
+                createAccount(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     private void signIn(String email, String password){
         //sign in the recurrent user with email and password previously created.
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() { //add to listener
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (!task.isSuccessful()) { //when failed
-                    Toast.makeText(SecondActivity.this, "SignIn--Authentication failed.",Toast.LENGTH_LONG).show();
-                } else {
-                    //return to MainActivity is login works
-                    finish();
-                }
-            }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("", "signInWithEmail:success");
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SecondActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
 
-        });
+                        // ...
+                    }
+                });
     }
 
     private void createAccount(String email, String password) {
         //create account for new users
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {  //update listener.
-                if (!task.isSuccessful()) { //when failed
-                    Toast.makeText(SecondActivity.this, "createAccount--Authentication failed.",Toast.LENGTH_LONG).show();
-                } else {
-                    //return to MainActivity is login works
-                    finish();
-                }
-            }
-        });
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("", "createUserWithEmail:success");
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SecondActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
     }
 
 
